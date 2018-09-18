0821527dd581a108ce1afaa5e9a5c2d9b27fccbf
machaku <bmachaku@gmail.com>
2018-09-18 17:20:03 +0300

    update htc appconfig

---
+++ hivs_htc/__init__.py
@@ -0,0 +1,2 @@
+__version__ = '0.1.0'
+default_app_config = 'hivs_htc.apps.HivsHtcConfig'
--- hivs_htc/apps.py
+++ hivs_htc/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class HivsHtcConfig(AppConfig):
     name = 'hivs_htc'
+    verbose_name = 'HIV Testing and Councelling'
