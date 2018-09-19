88486334edb01be73d98e457b07cc10c6cb0cad7
machaku <bmachaku@gmail.com>
2018-09-19 23:00:21 +0300

    update admin

--- hivs_administrative/admin.py
+++ hivs_administrative/admin.py
@@ -3,9 +3,10 @@ from django.apps import apps
 from django_mptt_admin.admin import DjangoMpttAdmin
 from import_export.admin import ImportExportModelAdmin
 from hivs_utils.admin import BaseAdmin
-from .import_export import AreaResource, StreetResource
+from .import_export import AreaTypeResource, AreaResource, StreetResource
 
 
+AreaType = apps.get_registered_model('hivs_administrative', 'AreaType')
 Area = apps.get_registered_model('hivs_administrative', 'Area')
 Street = apps.get_registered_model('hivs_administrative', 'Street')
 
@@ -13,20 +14,28 @@ Street = apps.get_registered_model('hivs_administrative', 'Street')
 class AdministrativeAdmin(BaseAdmin, admin.OSMGeoAdmin, ImportExportModelAdmin):
     ordering = ['name']
     readonly_fields = ['id', 'timestamp', 'last_modified']
-    list_display = ['id', 'code', 'name', 'location_description_auto']
+    list_display = ['id', 'name', 'location_description_auto']
     list_display_links = ['id', 'name']
     search_fields = ['name', 'id', 'code']
 
 
+@admin.register(AreaType)
+class AreaTypeAdmin(AdministrativeAdmin):
+    resource_class = AreaTypeResource
+    list_display = ['id', 'name']
+    ordering = ['id']
+
+
+@admin.register(Area)
 class AreaAdmin(AdministrativeAdmin, DjangoMpttAdmin):
     resource_class = AreaResource
+    list_display = ['id', 'name', 'location_description_auto', 'area_type']
+    list_select_related = ['area_type']
+    list_filter = ['area_type']
     raw_id_fields = ['parent']
 
 
+@admin.register(Street)
 class StreetAdmin(AdministrativeAdmin):
     resource_class = StreetResource
     raw_id_fields = ['area']
-
-
-admin.site.register(Area, AreaAdmin)
-admin.site.register(Street, StreetAdmin)
--- hivs_administrative/import_export.py
+++ hivs_administrative/import_export.py
@@ -1,12 +1,23 @@
 from django.apps import apps
-from import_export import resources
+from import_export import resources, fields, widgets
 
 
 Area = apps.get_registered_model('hivs_administrative', 'Area')
 Street = apps.get_registered_model('hivs_administrative', 'Street')
+AreaType = apps.get_registered_model('hivs_administrative', 'AreaType')
+
+
+class AreaTypeResource(resources.ModelResource):
+
+    class Meta:
+        model = AreaType
 
 
 class AreaResource(resources.ModelResource):
+    parent = fields.Field(
+        attribute='parent',
+        widget=widgets.ForeignKeyWidget(Area)
+    )
 
     class Meta:
         model = Area
