7eebc4b1917add0990135bf7391dc171f433a551
machaku <bmachaku@gmail.com>
2018-10-05 16:32:47 +0300

    add client acquisition_date

--- hivs_clients/abstract_models.py
+++ hivs_clients/abstract_models.py
@@ -2,6 +2,7 @@ from django.contrib.gis.db import models
 from django.contrib.postgres.fields import JSONField, ArrayField
 from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
+from hivs_utils.datetime import today
 
 
 class AbstractProfile(models.Model):
@@ -10,6 +11,7 @@ class AbstractProfile(models.Model):
         max_length=255,
         blank=True
     )
+    acquisition_date = models.DateField(_('acquisition date'), default=today)
     first_name = models.CharField(_('first name'), max_length=255)
     last_name = models.CharField(_('last name'), max_length=255)
     middle_name = models.CharField(_('middle name'), max_length=255, blank=True)
