5ca4d2503d2b8c6bda498bdeeb5d8fca9da53611
machaku <bmachaku@gmail.com>
2018-10-24 22:20:41 +0300

    modify dash

--- hivs_dash/static/hivs_dash/js/index.js
+++ hivs_dash/static/hivs_dash/js/index.js
@@ -3,6 +3,7 @@
 var app = new Vue({
     el: '#app',
     data: {
+        clientsTotal: {count: null},
         dailyAquisition: {
             date: [],
             count: [],
@@ -27,7 +28,55 @@ var app = new Vue({
         lastMonthAquisition: {
             count: null,
             gender: {labels:[], data: []}
-        }
+        },
+        map: {
+            instance: null,
+            base: {
+                url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+                options: {
+                    attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, © <a href="https://carto.com/attribution">CARTO</a></div>'
+                }
+            },
+            level1: {
+                data: null,
+                options: {
+                    weight: 2,
+                    color: '#ECEFF1',
+                    opacity: 1,
+                    fillColor: '#FF0000',
+                    fillOpacity: 1
+                }
+            },
+            baseLayer: null,
+            level1Layer: null
+        },
+    },
+
+    methods: {
+        initMap() {
+            this.map.instance = L.map('clients-map').setView([0, 0], 7);
+            this.map.baseLayer = L.tileLayer(this.map.base.url, this.map.base.options);
+            this.map.baseLayer.addTo(this.map.instance);
+        },
+
+        initLayers() {
+            this.map.level1Layer = L.choropleth(this.map.level1.data.results, {
+                valueProperty: 'count',
+                scale: ['white', '#17a2b8'],
+                steps: 5,
+                mode: 'q', // q for quantile, e for equidistant, k for k-means
+                style: {
+                    color: '#fff',
+                    weight: 2,
+                    fillOpacity: 0.7
+                },
+                onEachFeature: function(feature, layer) {
+                    layer.bindPopup(
+                        feature.properties.name + ': ' + feature.properties.count + ' clients')
+                }
+            }).addTo(this.map.instance);
+            this.map.instance.fitBounds(this.map.level1Layer.getBounds());
+        },
     },
     mounted () {
         var now = new Date();
@@ -48,6 +97,7 @@ var app = new Vue({
                     this.dailyAquisition.date.push(obj.acquisition_date);
                     this.dailyAquisition.count.push(obj.count);
                 })
+
             })
             .catch(error => {console.log(error)})
 
@@ -118,5 +168,30 @@ var app = new Vue({
                 })
             })
             .catch(error => {console.log(error)})
+
+        // get administrative areas level 1
+        axios
+            .get(
+                apiRoot + 'administrative/areas/related_count/', {
+                params: {
+                    level: 1,
+                    by: 'profiles'
+                }
+            })
+            .then((response) => {
+                this.map.level1.data = response.data;
+                this.initMap();
+                this.initLayers();
+            })
+            .catch(error => {console.log(error)})
+
+        // total clients
+        axios
+            .get(apiRoot + 'clients/profiles/total/')
+            .then((response) => {
+                this.clientsTotal = response.data;
+            })
+            .catch(error => {console.log(error)})
+
     }
 })
