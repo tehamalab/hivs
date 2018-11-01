743904eb952de139df6bb020e9c120ab743d55e2
machaku <bmachaku@gmail.com>
2018-11-01 11:19:52 +0300

    add pp deliveries count pivot

--- hivs/api_urls.py
+++ hivs/api_urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from hivs_administrative.api.views import AreaTypeViewSet, AreaViewSet, StreetViewSet
 from hivs_clients.api.views import ProfileViewSet
-from hivs_pp.api.views import CategoryViewSet, ServiceViewSet, DeliveryViewSet
+from hivs_pp.api.views import CategoryViewSet, ServiceViewSet, DeliveryViewSet, DeliveryPivotViewSet
 
 
 router = routers.DefaultRouter()
@@ -13,6 +13,7 @@ router.register(r'clients/profiles', ProfileViewSet)
 router.register(r'prevention/categories', CategoryViewSet)
 router.register(r'prevention/services', ServiceViewSet)
 router.register(r'prevention/deliveries', DeliveryViewSet)
+router.register(r'prevention/pivot', DeliveryPivotViewSet, 'delivery-pivot')
 
 
 urlpatterns = [
--- hivs_pp/api/views.py
+++ hivs_pp/api/views.py
@@ -1,9 +1,11 @@
+import pandas as pd
 from django.apps import apps
 from django.db.models import Count
 from django.utils.translation import gettext_lazy as _
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.exceptions import APIException, MethodNotAllowed
 from rest_framework.decorators import action
 from .serializers import CategorySerializer, ServiceSerializer, DeliverySerializer
 
@@ -79,6 +81,54 @@ class DeliveryViewSet(viewsets.ModelViewSet):
         'provider',
     ]
 
+    def parse_count_request(self, request):
+        """
+        Parse request parameter for counting actions.
+        """
+
+        by = set(request.query_params.getlist('by'))
+        if not by:
+            raise APIException(_('Missing parameter `by`.'), status.HTTP_400_BAD_REQUEST)
+        if not by.issubset(self.count_by):
+            raise APIException(_('Invalid `by` value.'), status.HTTP_400_BAD_REQUEST)
+
+        area_level = None
+        if 'related_areas' in by and request.query_params.get('area_level'):
+            try:
+                area_level = int(request.query_params.get('area_level', ''))
+            except ValueError:
+                raise APIException(
+                    _('Invalid or Missing `area_level`.'),
+                    status.HTTP_400_BAD_REQUEST
+                )
+
+        return {'by': by, 'area_level': area_level}
+
+    def get_count(self, by, area_level=None):
+        """Count service deliveries by group."""
+
+        if area_level is not None:
+            by.remove('related_areas')
+            by |= {'area_id', 'area_name'}
+
+        # ordering causes duplicate entries if not part of grouping. See
+        # https://docs.djangoproject.com/en/dev/topics/db/aggregation/#interaction-with-default-ordering-or-order-by
+        for i in self.ordering:
+            if i not in by:
+                self.ordering.remove(i)
+
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if area_level is not None:
+            areas = Area.objects.filter(level=area_level).values_list('id', flat=True)
+            queryset = queryset\
+                .filter(related_areas_ids__overlap=list(areas))\
+                .extra(select={'area_id': 'related_areas_ids[%s]'}, select_params=(area_level + 1,))\
+                .extra(select={'area_name': 'related_areas[%s]'}, select_params=(area_level + 1,))
+
+        data = queryset.values(*by).annotate(count=Count('id'))[:100]
+        return data
+
     @action(detail=False, methods=['get'])
     def total(self, request, *args, **kwargs):
         """Total number of service deliveries."""
@@ -89,43 +139,110 @@ class DeliveryViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'])
     def count(self, request, *args, **kwargs):
         """Count service deliveries by group."""
+        params = self.parse_count_request(request)
+        return Response(self.get_count(params['by'], params['area_level']))
 
-        area_level = None
 
-        by = set(request.query_params.getlist('by'))
+class DeliveryPivotViewSet(DeliveryViewSet):
+    """
+    Prevention interventions Service Delivery Pivot Table.
+    """
 
-        if not by:
-            return Response({'detail': _('Missing parameter `by`.')}, status.HTTP_400_BAD_REQUEST)
+    rows_fields = ['date', 'gender', 'age', 'area', 'area_name', 'services', 'referral_made',
+                   'referral_successful']
+    columns_fields = ['date', 'gender', 'age', 'area', 'services', 'referral_made',
+                      'referral_successful', 'count']
+    values_fields = ['count']
+    aggregations = ['mean', 'sum', 'count']
 
-        if not by.issubset(self.count_by):
-            return Response({'detail': _('Invalid `by` value.')}, status.HTTP_400_BAD_REQUEST)
+    def parse_pivot_request(self, request):
+        """Parse request parameter for pivot table generation."""
+        rows = set(request.query_params.getlist('rows'))
+        if not rows:
+            raise APIException(_('Missing parameter `rows`.'), status.HTTP_400_BAD_REQUEST)
+        if not rows.issubset(self.rows_fields):
+            raise APIException(_('Invalid `rows` value.'), status.HTTP_400_BAD_REQUEST)
 
-        if 'related_areas' in by and request.query_params.get('area_level'):
-            try:
-                area_level = int(request.query_params.get('area_level', ''))
-            except ValueError:
-                return Response(
-                    {'detail': _('Invalid or Missing `area_level`.')},
-                    status.HTTP_400_BAD_REQUEST
-                )
+        columns = set(request.query_params.getlist('columns'))
+        if not columns:
+            raise APIException(_('Missing parameter `columns`.'), status.HTTP_400_BAD_REQUEST)
+        if not columns.issubset(self.columns_fields):
+            raise APIException(_('Invalid `columns` value.'), status.HTTP_400_BAD_REQUEST)
 
-        if area_level is not None:
-            by.remove('related_areas')
-            by |= {'area_id', 'area_name'}
+        values = set(request.query_params.getlist('values'))
+        if not values:
+            raise APIException(_('Missing parameter `values`.'), status.HTTP_400_BAD_REQUEST)
+        if not values.issubset(self.values_fields):
+            raise APIException(_('Invalid `values` value.'), status.HTTP_400_BAD_REQUEST)
 
-        # ordering causes duplicate entries if not part of grouping. See
-        # https://docs.djangoproject.com/en/dev/topics/db/aggregation/#interaction-with-default-ordering-or-order-by
-        self.ordering = by
+        try:
+            fill = int(self.request.query_params.get('fill', 0))
+        except ValueError:
+            raise APIException(_('Invalid `fill` value.'), status.HTTP_400_BAD_REQUEST)
 
-        queryset = self.filter_queryset(self.get_queryset())
+        agg = request.query_params.get('aggregation', 'mean')
+        if agg not in self.aggregations:
+            raise APIException(_('Invalid `aggregation`.'), status.HTTP_400_BAD_REQUEST)
 
-        if area_level is not None:
-            areas = Area.objects.filter(level=area_level).values_list('id', flat=True)
-            queryset = queryset\
-                .filter(related_areas_ids__overlap=list(areas))\
-                .extra(select={'area_id': 'related_areas_ids[%s]'}, select_params=(area_level + 1,))\
-                .extra(select={'area_name': 'related_areas[%s]'}, select_params=(area_level + 1,))
-            print(by)
+        totals_label = request.query_params.get('totals')
+        if totals_label:
+            include_totals = True
+        else:
+            include_totals = False
 
-        data = queryset.values(*by).annotate(count=Count('id'))[:100]
-        return Response(data)
+        return {
+            'index': list(rows),
+            'columns': list(columns),
+            'values': list(values),
+            'fill_value': fill,
+            'margins': include_totals,
+            'margins_name': totals_label,
+            'aggfunc': agg,
+        }
+
+    def list(self, request, *args, **kwargs):
+        raise MethodNotAllowed(request.method)
+
+    def create(self, request):
+        raise MethodNotAllowed(request.method)
+
+    def retrieve(self, request, pk=None):
+        raise MethodNotAllowed(request.method)
+
+    def update(self, request, pk=None):
+        raise MethodNotAllowed(request.method)
+
+    def partial_update(self, request, pk=None):
+        raise MethodNotAllowed(request.method)
+
+    def destroy(self, request, pk=None):
+        raise MethodNotAllowed(request.method)
+
+    @action(detail=False, methods=['get'])
+    def count(self, request, *args, **kwargs):
+        """Count and pivot service deliveries by group."""
+        count_params = self.parse_count_request(request)
+        pivot_params = self.parse_pivot_request(request)
+
+        counts = self.get_count(count_params['by'], count_params['area_level'])
+        df = pd.DataFrame(list(counts))
+
+        # Convert all column values to string to simplify flatten the pivot table
+        for col in pivot_params['columns']:
+            df[col] = df[col].astype(str)
+
+        # pivot
+        df = pd.pivot_table(
+            df,
+            index=pivot_params['index'],
+            columns=pivot_params['columns'],
+            values=pivot_params['values'],
+            fill_value=pivot_params['fill_value'],
+            margins=pivot_params['margins'],
+            margins_name=pivot_params['margins_name'],
+            aggfunc=pivot_params['aggfunc'],
+        )
+
+        # flatten the pivot
+        df.columns = df.columns.to_series().str.join('_')
+        return Response(df.to_dict('index'))
