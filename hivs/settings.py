5cfdca54d95dfe9e2b04e4d0cb131f1afaef48da
machaku <bmachaku@gmail.com>
2018-11-12 12:08:56 +0300

    add token authentication

--- hivs/settings.py
+++ hivs/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'rest_framework',
     'rest_framework_gis',
+    'rest_framework.authtoken',
     'django_filters',
     'rangefilter',
     'hivs_utils',
@@ -222,6 +223,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.DjangoModelPermissions',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
